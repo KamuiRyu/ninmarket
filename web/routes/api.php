09eeff977d5c4a119aebfb8b996589fda73f3810
KamuiRyu <koushirogamer@gmail.com>
2023-06-16 19:03:47 -0300

    Set laravel to work only with requests (API mode)

--- web/routes/api.php
+++ web/routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->group(function () {
+    Route::get('/users', function () {
+        return response()->json(['message' => 'API Users Endpoint']);
+    });
+    // Defina outras rotas da sua API aqui
 });
